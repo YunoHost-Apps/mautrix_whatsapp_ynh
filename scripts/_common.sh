df71d9163600affe987d53954c73f0d5bbd05131
Gredin67 <35761345+Gredin67@users.noreply.github.com>
2020-05-02 21:21:02 +0200

    add golang dep

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies=""
+pkg_dependencies="golang"
 GO_VERSION="1.11"
 GO_PATH="/usr/lib/go-$GO_VERSION/bin"
 
