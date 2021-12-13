798fc4969b0d3ec69ecd17166f5ffd2dc2709b83
Gredin67 <gredin67@sans-nuage.fr>
2021-12-14 00:40:24 +0100

    install pre-built (#39)
    * install pre-built

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,8 +6,6 @@
 
 # dependencies used by the app
 pkg_dependencies="g++ postgresql ffmpeg"
-GO_VERSION="1.17"
-GO_PATH="/usr/lib/go-$GO_VERSION/bin"
 
 #=================================================
 # PERSONAL HELPERS
@@ -16,12 +14,15 @@ GO_PATH="/usr/lib/go-$GO_VERSION/bin"
 # Detect the system architecture to download the right tarball
 # NOTE: `uname -m` is more accurate and universal than `arch`
 # See https://en.wikipedia.org/wiki/Uname
-if [ -n "$(uname -m | grep 64)" ]; then
+
+if [ -n "$(uname -m | grep arm64)" ]; then
+	architecture="arm64"
+elif [ -n "$(uname -m | grep 64)" ]; then
 	architecture="amd64"
-elif [ -n "$(uname -m | grep 86)" ]; then
-	architecture="386"
+# elif [ -n "$(uname -m | grep 86)" ]; then
+# 	architecture="386"
 elif [ -n "$(uname -m | grep arm)" ]; then
-	architecture="armv6l"
+	architecture="arm"
 else
 	ynh_die "Unable to detect your achitecture, please open a bug describing \
         your hardware and the result of the command \"uname -m\"." 1
