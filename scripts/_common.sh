f781d6b136b9935e5fd0583520a5258ab95d015a
Gredin67 <gredin67@sans-nuage.fr>
2022-01-02 17:23:30 +0100

    remove detect archi

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,23 +11,6 @@ pkg_dependencies="g++ postgresql ffmpeg"
 # PERSONAL HELPERS
 #=================================================
 
-# Detect the system architecture to download the right tarball
-# NOTE: `uname -m` is more accurate and universal than `arch`
-# See https://en.wikipedia.org/wiki/Uname
-
-if [ -n "$(uname -m | grep arm64)" ]; then
-	architecture="arm64"
-elif [ -n "$(uname -m | grep 64)" ]; then
-	architecture="amd64"
-# elif [ -n "$(uname -m | grep 86)" ]; then
-# 	architecture="386"
-elif [ -n "$(uname -m | grep arm)" ]; then
-	architecture="arm"
-else
-	ynh_die "Unable to detect your achitecture, please open a bug describing \
-        your hardware and the result of the command \"uname -m\"." 1
-fi
-
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
