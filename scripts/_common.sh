f2f21b8ff07f2783bc3702139625736cbcd266d9
Dante <thardev@users.noreply.github.com>
2023-06-04 21:30:13 +0100

    Packaging v2 WIP

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
-#=================================================
-
-# dependencies used by the app (must be on a single line)
-pkg_dependencies="g++ postgresql ffmpeg"
-
-#=================================================
 # CONFIG PANEL SETTERS
 #=================================================
 
@@ -22,24 +15,24 @@ apply_permissions() {
     then
         #ynh_systemd_action --service_name="$app" --action=stop
         # Get all entries between "permissions:" and "relay:" keys, remove the role part, remove commented parts, format it with newlines and clean whitespaces and double quotes.
-        allDefinedEntries=$(awk '/permissions:/{flag=1; next} /relay:/{flag=0} flag' "$final_path/config.yaml" | sed "/: $role/d" | sed -r 's/: (admin|user|relay)//' | tr -d '[:blank:]' | sed '/^#/d' | tr -d '\"' | tr ',' '\n' )
+        allDefinedEntries=$(awk '/permissions:/{flag=1; next} /relay:/{flag=0} flag' "$install_dir/config.yaml" | sed "/: $role/d" | sed -r 's/: (admin|user|relay)//' | tr -d '[:blank:]' | sed '/^#/d' | tr -d '\"' | tr ',' '\n' )
         # Delete everything from the corresponding role to insert the new defined values. This way we also handle deletion of users.
-        sed -i "/permissions:/,/relay:/{/: $role/d;}" "$final_path/config.yaml"
+        sed -i "/permissions:/,/relay:/{/: $role/d;}" "$install_dir/config.yaml"
         # Ensure that entries with value surrounded with quotes are deleted too. E.g. "users".
-        sed -i "/permissions:/,/relay:/{/: \"$role\"/d;}" "$final_path/config.yaml"
+        sed -i "/permissions:/,/relay:/{/: \"$role\"/d;}" "$install_dir/config.yaml"
       	for user in "${usersArray[@]}"
             do
               if grep -q -x "${user}" <<< "$allDefinedEntries"
               then
                 ynh_print_info "User $user already defined in another role."
               else
-                sed -i "/permissions:/a \        \\\"$user\": $role" "$final_path/config.yaml" # Whitespaces are needed so that the file can be correctly parsed
+                sed -i "/permissions:/a \        \\\"$user\": $role" "$install_dir/config.yaml" # Whitespaces are needed so that the file can be correctly parsed
               fi
         done
     fi
     set +o noglob
 
-    ynh_print_info "Users with role $role added in $final_path/config.yaml"
+    ynh_print_info "Users with role $role added in $install_dir/config.yaml"
 }
 
 
@@ -48,19 +41,19 @@ set__listuser() {
   role="user"
   ynh_app_setting_set --app=$app --key=listuser --value="$listuser"
   apply_permissions
-  ynh_store_file_checksum --file="$final_path/config.yaml"
+  ynh_store_file_checksum --file="$install_dir/config.yaml"
 }
 
 set__listrelay() {
   role="relay"
   ynh_app_setting_set --app=$app --key=listrelay --value="$listrelay"
   apply_permissions
-  ynh_store_file_checksum --file="$final_path/config.yaml"
+  ynh_store_file_checksum --file="$install_dir/config.yaml"
 }
 
 set__listadmin() {
   role="admin"
   ynh_app_setting_set --app=$app --key=listadmin --value="$listadmin"
   apply_permissions
-  ynh_store_file_checksum --file="$final_path/config.yaml"
+  ynh_store_file_checksum --file="$install_dir/config.yaml"
 }
